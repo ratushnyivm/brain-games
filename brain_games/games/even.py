f66074808f71b4278199cf8811fc56828eebf782
ratushnyyvm <ratushnyyvm@gmail.com>
2022-06-12 17:17:10 +0300

    refactoring even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,40 +1,18 @@
-import prompt
 import random
 
 
 def even():
-    print('Welcome to the Brain Games!')
+    output = dict.fromkeys(['game_condition', 'question', 'right_answer'])
 
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    output['game_condition'] = 'Answer "yes" if the number is even, '\
+        'otherwise answer "no".'
 
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    question = random.randrange(100)
 
-    for i in range(3):
-        question = random.randrange(100)
-        print(f'Question: {question}')
-        answer = prompt.string('Your answer: ')
+    output['question'] = question
 
-        if_correct = 'Correct!'
+    right_answer = "yes" if question % 2 == 0 else "no"
 
-        if question % 2 == 0:
-            if answer == 'yes':
-                print(if_correct)
+    output['right_answer'] = right_answer
 
-            else:
-                return print(
-                    f'"{answer}" is wrong answer ;(. Correct answer was "yes".'
-                    f'\nLet\'s try again, {name}!'
-                )
-
-        else:
-            if answer == 'no':
-                print(if_correct)
-
-            else:
-                return print(
-                    f'"{answer}" is wrong answer ;(. Correct answer was "no".'
-                    f'\nLet\'s try again, {name}!'
-                )
-
-    return print(f"Congratulations, {name}!")
+    return output
