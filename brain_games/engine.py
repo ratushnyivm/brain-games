65363eb331122928452401a698f681ab6ffd10a4
ratushnyyvm <ratushnyyvm@gmail.com>
2022-08-08 21:14:05 +0300

    upd brain_games/engine.py: add a color for correct/incorrect answer messages

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,9 @@
+from colorama import init, Fore
 import prompt
 
 
 def engine(game_module, number_of_rounds=3):
+    init(autoreset=True)
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
@@ -10,12 +12,12 @@ def engine(game_module, number_of_rounds=3):
         output_of_game_module = game_module()
         print(f"Question: {output_of_game_module['question']}")
         answer = prompt.string('Your answer: ')
-        if_right = 'Correct!'
+        if_right = "Correct!"
         if_wrong = f"'{answer}' is wrong answer ;(. "\
-            f"Correct answer was '{output_of_game_module['right_answer']}'."\
-            f"\nLet's try again, {name}!"
+            f"Correct answer was '{output_of_game_module['right_answer']}'."
         if answer == output_of_game_module['right_answer']:
-            print(if_right)
+            print(Fore.LIGHTGREEN_EX + if_right)
         else:
-            return print(if_wrong)
+            print(Fore.LIGHTRED_EX + if_wrong)
+            return print(f"Let's try again, {name}!")
     return print(f"Congratulations, {name}!")
