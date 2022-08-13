c8ca4d537c5e8ca6f67dd3464e6ed1919ba4b62d
ratushnyyvm <ratushnyyvm@gmail.com>
2022-08-13 18:54:53 +0300

    refactoring: engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,20 +4,28 @@ import prompt
 
 def engine(game_module, number_of_rounds=3):
     init(autoreset=True)
+
     print('Welcome to the Brain Games!')
+
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game_module()['game_condition'])
+
+    print(game_module()['description'])
+
     for i in range(number_of_rounds):
         output_of_game_module = game_module()
+
         print(f"Question: {output_of_game_module['question']}")
         answer = prompt.string('Your answer: ')
+
         if_right = "Correct!"
         if_wrong = f"'{answer}' is wrong answer ;(. "\
             f"Correct answer was '{output_of_game_module['right_answer']}'."
+
         if answer == output_of_game_module['right_answer']:
             print(Fore.LIGHTGREEN_EX + if_right)
         else:
             print(Fore.LIGHTRED_EX + if_wrong)
             return print(f"Let's try again, {name}!")
+
     return print(f"Congratulations, {name}!")
