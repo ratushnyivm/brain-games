fdc94561f1a5ec91cfd41d78a120db50aaeebfd0
ratushnyyvm <ratushnyyvm@gmail.com>
2022-08-13 21:20:29 +0300

    add test-gcd

--- tests/test_games.py
+++ tests/test_games.py
@@ -1,4 +1,5 @@
-from brain_games.games import calc, even
+from brain_games.games import calc, even, gcd
+import math
 
 DESCRIPTION = 'description'
 QUESTION = 'question'
@@ -28,8 +29,32 @@ def test_calc():
 
 def test_even():
     output = even.even()
+    question = output.get(QUESTION)
+    right_answer = output.get(RIGHT_ANSWER)
 
     assert type(output) is dict
     assert type(output.get(DESCRIPTION)) is str
-    assert type(output.get(QUESTION)) is int
+    assert type(question) is int
+    assert type(right_answer) is str
+
+    if question % 2 == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+
+    assert answer == right_answer
+
+
+def test_gcd():
+    output = gcd.gcd()
+
+    assert type(output) is dict
+    assert type(output.get(DESCRIPTION)) is str
+    assert type(output.get(QUESTION)) is str
     assert type(output.get(RIGHT_ANSWER)) is str
+
+    question = output.get(QUESTION).split()
+    right_answer = int(output.get(RIGHT_ANSWER))
+    answer = math.gcd(int(question[0]), int(question[1]))
+
+    assert answer == right_answer
